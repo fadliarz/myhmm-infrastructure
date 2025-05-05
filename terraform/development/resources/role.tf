e9cb728529a53e7fed537c1f0fe6fac5d6c7450c
fadliarz <fadliarz03@gmail.com>
2025-05-05 17:25:16 +0700

    feat(class): add iam role and iam policy for the corresponding lambda and pipe

--- terraform/development/resources/role.tf
+++ terraform/development/resources/role.tf
@@ -254,6 +254,70 @@ resource "aws_iam_role_policy_attachment" "lesson-pipe-iam-role-policy-attachmen
  */
 
 
+resource "aws_iam_role" "class-pipe-iam-role" {
+  name = var.class_pipe_iam_role_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = "PipesPermissions"
+        Principal = {
+          Service = "pipes.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_policy" "class-pipe-iam-policy" {
+  name = var.class_pipe_iam_policy_name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Sid" : "dynamodbPermissions",
+        "Effect" : "Allow",
+        "Action" : [
+          "dynamodb:DescribeStream",
+          "dynamodb:GetRecords",
+          "dynamodb:GetShardIterator",
+          "dynamodb:ListStreams"
+        ],
+        "Resource" : [
+          aws_dynamodb_table.class-table.stream_arn
+        ]
+      },
+      {
+        "Sid" : "sqsPermissions",
+        "Effect" : "Allow",
+        "Action" : [
+          "sqs:SendMessage"
+        ],
+        "Resource" : [
+          aws_sqs_queue.class-queue.arn,
+          aws_sqs_queue.class-dlq.arn
+        ]
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "class-pipe-iam-role-policy-attachment" {
+  role       = aws_iam_role.class-pipe-iam-role.name
+  policy_arn = aws_iam_policy.class-pipe-iam-policy.arn
+}
+
+
+/**
+
+
+ */
+
+
 resource "aws_iam_role" "class-assignment-lambda-iam-role" {
   name = var.class_assignment_lambda_iam_role_name
   assume_role_policy = jsonencode({
@@ -567,6 +631,83 @@ resource "aws_iam_role_policy_attachment" "lesson-lambda-iam-role-policy-attachm
  */
 
 
+resource "aws_iam_role" "class-lambda-iam-role" {
+  name = var.class_lambda_iam_role_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_policy" "class-lambda-iam-policy" {
+  name = var.class_lambda_iam_policy_name
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "sqsPermissions",
+        "Effect" : "Allow",
+        "Action" : [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes"
+        ],
+        "Resource" : [
+          aws_sqs_queue.class-queue.arn,
+          aws_sqs_queue.class-dlq.arn
+        ]
+      },
+      {
+        "Sid" : "dynamodbPermissions",
+        "Effect" : "Allow",
+        "Action" : [
+          "dynamodb:PutItem",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:Get*",
+          "dynamodb:Update*",
+          "dynamodb:Delete*",
+        ],
+        "Resource" : [
+          aws_dynamodb_table.class-assignment-table.arn,
+        ]
+      },
+      {
+        "Sid" : "cloudwatchPermissions",
+        "Effect" : "Allow",
+        "Action" : [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "class-lambda-iam-role-policy-attachment" {
+  role       = aws_iam_role.class-lambda-iam-role.name
+  policy_arn = aws_iam_policy.class-lambda-iam-policy.arn
+}
+
+
+/**
+
+
+ */
+
+
 variable "class_assignment_pipe_iam_role_name" {
   type    = string
   default = "CLASS_ASSIGNMENT_PIPE_IAM_ROLE"
@@ -678,4 +819,34 @@ variable "lesson_lambda_iam_role_name" {
 variable "lesson_lambda_iam_policy_name" {
   type    = string
   default = "LESSON_LAMBDA_IAM_POLICY"
+}
+
+
+/**
+
+
+ */
+
+
+variable "class_pipe_iam_role_name" {
+  type    = string
+  default = "CLASS_PIPE_IAM_ROLE"
+}
+
+
+variable "class_pipe_iam_policy_name" {
+  type    = string
+  default = "CLASS_PIPE_IAM_POLICY"
+}
+
+
+variable "class_lambda_iam_role_name" {
+  type    = string
+  default = "CLASS_LAMBDA_IAM_ROLE"
+}
+
+
+variable "class_lambda_iam_policy_name" {
+  type    = string
+  default = "CLASS_LAMBDA_IAM_POLICY"
 }
