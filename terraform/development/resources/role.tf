a161975885ee2d89cf2038079d255732eb931d6f
fadliarz <fadliarz03@gmail.com>
2025-05-09 23:30:40 +0700

    feat(tag): add iam role and iam policy for the corresponding lambda and pipe

--- terraform/development/resources/role.tf
+++ terraform/development/resources/role.tf
@@ -708,6 +708,142 @@ resource "aws_iam_role_policy_attachment" "enrollment-and-class-assignment-pipe-
  */
 
 
+resource "aws_iam_role" "tag-lambda-iam-role" {
+  name = var.tag_lambda_iam_role_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+
+resource "aws_iam_policy" "tag-lambda-iam-policy" {
+  name = var.tag_lambda_iam_policy_name
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "sqsPermissions",
+        "Effect" : "Allow",
+        "Action" : [
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes"
+        ],
+        "Resource" : [
+          aws_sqs_queue.tag-queue.arn,
+          aws_sqs_queue.tag-dlq.arn
+        ]
+      },
+      {
+        "Sid" : "dynamodbPermissions",
+        "Effect" : "Allow",
+        "Action" : [
+          "dynamodb:PutItem",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:Get*",
+          "dynamodb:Update*",
+          "dynamodb:Delete*",
+        ],
+        "Resource" : [
+          aws_dynamodb_table.tag-table.arn,
+          aws_dynamodb_table.scholarship-table.arn,
+        ]
+      },
+      {
+        "Sid" : "cloudwatchPermissions",
+        "Effect" : "Allow",
+        "Action" : [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "tag-lambda-iam-role-policy-attachment" {
+  role       = aws_iam_role.tag-lambda-iam-role.name
+  policy_arn = aws_iam_policy.tag-lambda-iam-policy.arn
+}
+
+
+resource "aws_iam_role" "tag-pipe-iam-role" {
+  name = var.tag_pipe_iam_role_name
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = "PipesPermissions"
+        Principal = {
+          Service = "pipes.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_policy" "tag-pipe-iam-policy" {
+  name = var.tag_pipe_iam_policy_name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Sid" : "dynamodbPermissions",
+        "Effect" : "Allow",
+        "Action" : [
+          "dynamodb:DescribeStream",
+          "dynamodb:GetRecords",
+          "dynamodb:GetShardIterator",
+          "dynamodb:ListStreams"
+        ],
+        "Resource" : [
+          aws_dynamodb_table.tag-table.stream_arn
+        ]
+      },
+      {
+        "Sid" : "sqsPermissions",
+        "Effect" : "Allow",
+        "Action" : [
+          "sqs:SendMessage"
+        ],
+        "Resource" : [
+          aws_sqs_queue.tag-queue.arn,
+          aws_sqs_queue.tag-dlq.arn
+        ]
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "tag-pipe-iam-role-policy-attachment" {
+  role       = aws_iam_role.tag-pipe-iam-role.name
+  policy_arn = aws_iam_policy.tag-pipe-iam-policy.arn
+}
+
+
+/**
+
+
+ */
+
+
 variable "course_pipe_iam_role_name" {
   type    = string
   default = "COURSE_PIPE_IAM_ROLE"
@@ -849,4 +985,34 @@ variable "enrollment_and_class_assignment_lambda_iam_role_name" {
 variable "enrollment_and_class_assignment_lambda_iam_policy_name" {
   type    = string
   default = "ENROLLMENT_AND_CLASS_ASSIGNMENT_LAMBDA_IAM_POLICY"
+}
+
+
+/**
+
+
+ */
+
+
+variable "tag_pipe_iam_role_name" {
+  type    = string
+  default = "TAG_PIPE_IAM_ROLE"
+}
+
+
+variable "tag_pipe_iam_policy_name" {
+  type    = string
+  default = "TAG_PIPE_IAM_POLICY"
+}
+
+
+variable "tag_lambda_iam_role_name" {
+  type    = string
+  default = "TAG_LAMBDA_IAM_ROLE"
+}
+
+
+variable "tag_lambda_iam_policy_name" {
+  type    = string
+  default = "TAG_LAMBDA_IAM_POLICY"
 }
